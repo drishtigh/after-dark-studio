c82a67d34731bb58ed03680159baba67618478e2
@drishtigh <drishti.chauhan@connect.polyu.hk>
2026-06-30 18:45:14 +0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://afterdarkstudiohk.com',
   integrations: [
     react(),
     mdx(),
-  ]
+    sitemap(),
+  ],
 });
